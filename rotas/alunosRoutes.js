7528b026443447058d84abd17565d3e767a2191d
GuilhermeBissi <20233005484@estudantes.ifpr.edu.br>
2025-09-08 11:35:28 -0300

    commit

--- rotas/alunosRoutes.js
+++ rotas/alunosRoutes.js
@@ -1,13 +1,20 @@
-// Exemplo correto do alunosRoutes.js
 const express = require('express');
 const router = express.Router();
-const { criarAluno, listarAlunos, buscarAluno, atualizarAluno, deletarAluno } = require('../controllers/alunosController');
 
-// Suas rotas aqui
-router.get('/', listarAlunos);
-router.get('/:id', buscarAluno);
-router.post('/', criarAluno);
-router.put('/:id', atualizarAluno);
-router.delete('/:id', deletarAluno);
+// Importar as funções do controller
+const { 
+    criarAluno, 
+    listarAlunos, 
+    buscarAluno, 
+    atualizarAluno, 
+    deletarAluno 
+} = require('../controllers/alunosController');
+
+// Definir as rotas
+router.get('/', listarAlunos);           // GET /api/alunos
+router.get('/:id', buscarAluno);         // GET /api/alunos/:id
+router.post('/', criarAluno);            // POST /api/alunos
+router.put('/:id', atualizarAluno);      // PUT /api/alunos/:id
+router.delete('/:id', deletarAluno);     // DELETE /api/alunos/:id
 
 module.exports = router;
